286052bf2a26ce9a0e4f7eb05d7fa21245600357
Josh <herz.josh@gmail.com>
2019-08-17 10:52:04 -0700

    Frontend now includes components for sections: aboutGame, recentUpdates, systemReq, and topReviews

---
+++ joshNotes.md
@@ -0,0 +1,14 @@
+** Installs **
+```npm install webpack webpack-cli @babel/cli @babel/core @babel/preset-react react react-dom express faker mongoose nodemon body-parser```
+
+** Test Data Seeding**
+1. start mongo
+```mongod --config /usr/local/etc/mongod.conf```
+run scripts
+``` npm start ```
+``` npm run seed```
+
+
+
+Bryans Skillz
+use 'cmd' + opt" + arrow for big cursor
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../db/index.js');
+
+const app = express();
+
+app.use(express.static('public/dist'))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('testing get request');
+});
+
+app.get('/api/reviews', function (req, res){
+  db.findTop3(function(err, data){
+    if(err){
+      console.log(err)
+    }else{
+      res.end(JSON.stringify(data));
+    }
+  })
+});
+
+const port = 3003;
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
